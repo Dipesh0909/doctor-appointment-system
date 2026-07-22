50b9b069ca26709166b062d6ebd2c527a5047165
Dipesh0909 <da436933@gmail.com>
2026-07-22 15:08:12 +0545

    fix patient table and create dashboards

---
+++ admin/dashboard.php
@@ -0,0 +1,8 @@
+<?php include "../includes/header.php"; ?>
+
+<div class="container mt-5">
+    <h2>Admin Dashboard</h2>
+    <p>Welcome Admin!</p>
+</div>
+
+<?php include "../includes/footer.php"; ?>
